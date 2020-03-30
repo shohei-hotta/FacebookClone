cafb468bdcc929b3d9b92485d070b3eb65b0d320
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-03-30 14:48:35 +0000

    写真の新規投稿機能を追加

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,11 +3,27 @@ class FeedsController < ApplicationController
   end
 
   def new
+    @feed = Feed.new
+  end
+
+  def create
+    @feed = Feed.new(feed_params)
+    if @feed.save
+      redirect_to feeds_path, notice: "写真を投稿しました。"
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:image, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'feeds/index'
-  get 'feeds/new'
-  get 'feeds/show'
-  get 'feeds/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "feeds#index"
+  resources :feeds
 end
